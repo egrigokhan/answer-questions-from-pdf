45adf9ec697d40317f891bdc3b316e8c82f88ecf
Gokhan Egri <egrigokhan@gmail.com>
2023-03-21 01:08:09 -0400

    Update index.py

--- lib/pdf/index.py
+++ lib/pdf/index.py
@@ -1,8 +1,8 @@
-from langchain.document_loaders import UnstructuredFileLoader
+from langchain.document_loaders import PyPDFLoader
 from langchain.chains.qa_with_sources import load_qa_with_sources_chain
 
 def loadAndChunkPDFFile(file_path):
-    loader = UnstructuredFileLoader(file_path)
+    loader = PyPDFLoader(file_path)
     docs = loader.load()
     
     chain = load_qa_with_sources_chain(llm, chain_type="stuff")
